c3fd42a3cddd86feb5a8a9b1aca2e659df0e8b9a
eharrison <evandro@itsdayoff.com>
2020-08-05 22:12:21 +0800

    Initial commit

---
+++ Example/dotLottieLoader/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  dotLottieLoader
+//
+//  Created by eharrison on 08/05/2020.
+//  Copyright (c) 2020 eharrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
