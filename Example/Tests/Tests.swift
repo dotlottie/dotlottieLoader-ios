e2171369c1e58b5505f8764bfea5ab87e5b344a7
Evandro <evandro.hoffmann@gmail.com>
2020-11-02 14:41:09 +0800

    chore: Added loading tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,28 +1,65 @@
 import XCTest
-import dotLottieLoader
+@testable import dotLottieLoader
 
 class Tests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testLoadDotLottie() {
+        let expectation = XCTestExpectation(description: "Load dotlottie file")
+            
+        DotLottieLoader.load(from: URL(string: "https://dotlottie.io/sample_files/animation.lottie")!) { (lottie) in
+            XCTAssertNotNil(lottie)
+            XCTAssertNotNil(lottie?.manifest)
+            
+            XCTAssertEqual(lottie?.manifest?.animations.count, lottie?.animations.count)
+            XCTAssertEqual("\(lottie?.manifest?.animations.first?.id ?? "").json", lottie?.animations.first?.lastPathComponent)
+            XCTAssertEqual(lottie?.animationUrl, lottie?.animations.first)
+            
+            XCTAssertEqual(lottie?.animations.count, 1)
+            XCTAssertEqual(lottie?.animations.first?.lastPathComponent, "lf20_gOmta2.json")
+            
+            XCTAssertEqual(lottie?.images.count, 0)
+            
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
     
+    func testLoadDotLottieWithExternalImage() {
+        let expectation = XCTestExpectation(description: "Load dotlottie file with external image")
+            
+        DotLottieLoader.load(from: URL(string: "https://dotlottie.io/sample_files/animation-external-image.lottie")!) { (lottie) in
+            XCTAssertNotNil(lottie)
+            XCTAssertNotNil(lottie?.manifest)
+            
+            XCTAssertEqual(lottie?.animations.count, 1)
+            XCTAssertEqual(lottie?.animations.first?.lastPathComponent, "with_image.json")
+            
+            XCTAssertEqual(lottie?.images.count, 1)
+            XCTAssertEqual(lottie?.images.first?.lastPathComponent, "img_0.png")
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testLoadDotLottieWithInlineImage() {
+        let expectation = XCTestExpectation(description: "Load dotlottie file with inline image")
+            
+        DotLottieLoader.load(from: URL(string: "https://dotlottie.io/sample_files/animation-inline-image.lottie")!) { (lottie) in
+            XCTAssertNotNil(lottie)
+            XCTAssertNotNil(lottie?.manifest)
+            
+            XCTAssertEqual(lottie?.animations.count, 1)
+            XCTAssertEqual(lottie?.animations.first?.lastPathComponent, "lf20_I2I090.json")
+            
+            XCTAssertEqual(lottie?.images.count, 1)
+            XCTAssertEqual(lottie?.images.first?.lastPathComponent, "image_0.png")
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
 }
